97ae98ee53b44759ba794d9745778d68526741ff
Faraz Ali <farazofficial9028@gmail.com>
2025-02-19 21:44:40 +0530

    refactored the code

--- src/app/dashboard/info/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const info = () => {
-    return (
-        <div>info Page</div>
-    )
-}
-
-export default info
--- src/app/dashboard/user/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-// Dynamic routing
-
-const page = ({ params }: { params: { id: string } }) => {
-    return (
-        <div >Hello This is  <span className='text-orange-500'>{params.id}</span></div>
-    )
-}
-
-export default page
